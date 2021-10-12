b5d782edebe74eac5c49a8dd5d5d598d6b312b64
solc21 <88914949+solc21@users.noreply.github.com>
2021-10-12 19:25:41 -0300

    Se elimina algunos if

--- src/ar/edu/unlam/pa/polinomio/Polinomio.java
+++ src/ar/edu/unlam/pa/polinomio/Polinomio.java
@@ -1,7 +1,6 @@
 package ar.edu.unlam.pa.polinomio;
 
 public class Polinomio {
-	
 	private int grado;
 	private double[] coeficientes;
 		
@@ -10,24 +9,18 @@ public class Polinomio {
 		this.coeficientes = coeficientes;
 	}
 	
-	
 	public double evaluarMSucesivas(double x) {
-		
 		double total = coeficientes[grado];
 		
-		if(grado < 1)
-			return total;
-		
-		for(int i = grado; i>0; i--) {
+		for(int i = grado; i>0 && grado >=1 ; i--) {
 			total += coeficientes[grado - i] * potencia(x, i);
 		}
-		
 		return total;
 	}
 	
 	private double potencia(double x, int n) {
-		
 		double resultado = x;
+		
 		while(n>1) {
 			resultado *= x;
 			n--;
@@ -35,68 +28,47 @@ public class Polinomio {
 		return resultado;
 	}
 	
-	
-	
 	public double evaluarRecursiva(double x){
 		double total = coeficientes[grado];
 		
-		if(grado < 1)
-			return total;
-		
-		for(int i = grado; i>0; i--) {
+		for(int i = grado; i>0 && grado >=1; i--) {
 			total += coeficientes[grado - i] * potenciaRecurs(x, i);
 		}
 		return total;
 	}
 	
 	private double potenciaRecurs(double x, int n) {
-		if(n==1) {
-			return x;
-		}
-		return x * potenciaRecurs(x, n-1);
+		return n==1? x : x * potenciaRecurs(x, n-1);
 	}
 	
-	
-
 	public double evaluarRecursivaPar(double x) {
 		double total = coeficientes[grado];
 		
-		if(grado < 1)
-			return total;
-		
-		for(int i = grado; i>0; i--) {
+		for(int i = grado; i>0 && grado >= 1; i--) {
 			total += coeficientes[grado - i] * potenciaRecursPar(x, i);
 		}
 		return total;
 	}
 	
 	private double potenciaRecursPar(double x, int n) {
-		if(n==1) {
-			return x;
-		}
-		return n % 2 == 0 ? potenciaRecursPar(x*x, n/2) : x * potenciaRecursPar(x, n-1);
+		return n==1? x : (n % 2 == 0 ? potenciaRecursPar(x*x, n/2) : x * potenciaRecursPar(x, n-1));
 	}
-	
-	
 	
 	public double evaluarProgDinamica(double x){
 		double total = coeficientes[grado];
 		
-		if(grado < 1)
-			return total;
+		if (grado >= 1) {
+			double[] potencias = potenciaDinamica(x, grado);
 		
-		double[] potencias = potenciaDinamica(x, grado);
-		
-		for(int i = grado; i>0; i--) {
-			total += coeficientes[i-1] * potencias[grado-i];
+			for(int i = grado; i>0; i--) {
+				total += coeficientes[i-1] * potencias[grado-i];
+			}
 		}
-		
 		return total;
 	}
 	
 	private double[] potenciaDinamica(double x, int n) {
 		double[] potencias = new double[n];
-		
 		potencias[0] = x;
 		
 		for(int i = 1; i<n; i++) {
@@ -105,50 +77,32 @@ public class Polinomio {
 		return potencias;
 	}
 	
-	
-	
 	public double evaluarMejorada(double x) {
 		double potencias = x;
 		double total = coeficientes[grado];
 		
-		if(grado < 1)
-			return total;
-		
-		for(int i = grado; i>0; i--) {
+		for(int i = grado; i>0 && grado >=1; i--) {
 			total += coeficientes[i-1] * potencias;
 			potencias *= x;
 		}
 		return total;
 	}
 	
-	
-	
 	public double evaluarPow(double x){
 		double total = coeficientes[grado];
 		
-		if(grado < 1)
-			return total;
-		
-		for(int i = grado; i>0; i--) {
+		for(int i = grado; i>0 && grado >=1; i--) {
 			total += coeficientes[grado - i] * Math.pow(x, i);
 		}
-
 		return total;
 	}
 	
-	
-
 	public double evaluarHorner(double x) {
 		double total = coeficientes[0];
 		
-		if(grado < 1)
-			return total;
-		
-		for(int i = 1; i<coeficientes.length; i++) {
+		for(int i = 1; i<coeficientes.length && grado >=1; i++) {
 			total = coeficientes[i] + (total*x);
 		}
-		
 		return total;
 	}
-	
 }
